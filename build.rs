bc628387a81d730b460cc148b5919708c9b22820
Hazel <38921844+HazelTheWitch@users.noreply.github.com>
2023-04-22 15:58:38 -0700

    Remove windows only from build.rs

--- build.rs
+++ build.rs
@@ -1,4 +1,3 @@
 fn main() {
-    println!("cargo:rustc-env=LITCRYPT_ENCRYPT_KEY=MY-SECRET-SPELL");  // I couldn't get this to work without passing this as an environment variable
     println!("cargo:rustc-env=SECRET_ENV=Shhhhhh");
 }
